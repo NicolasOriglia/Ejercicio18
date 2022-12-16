91d47bf561db16bcc7a763901b9dac878b885d9c
Nicolas Origlia <noriglia@gmail.com>
2022-12-16 20:49:15 -0300

    guardando datos en memoria de browser

---
+++ index.js
@@ -0,0 +1,15 @@
+const nombre = 'Nicolas';
+const apellido = 'Origlia';
+
+const datos = {
+    nombre,
+    apellido
+};
+
+console.log(datos);
+
+//localStorage.setItem('datosPersonales', JSON.stringify(datos));
+
+//sessionStorage.setItem('datosPersonales', JSON.stringify(datos));
+
+//document.cookie = `datosPersonales=${JSON.stringify(datos)}`;
